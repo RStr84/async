5d951a0b4fac6ceb02015d70c393a1bc4b5aa79a
RaphaelStraschewski <r.Straschewski@edu.bbq.de>
2025-02-07 14:43:12 +0100

    Initial commit

---
+++ demo_get2.php
@@ -0,0 +1,9 @@
+<?php
+
+//mit ppp+tab erstellt
+//echo "<pre>";
+//print_r($_GET);
+//echo "</pre>";
+
+echo 'Hallo GET, ich bin ' . $_GET['fname'] . ' ' . $_GET['lname']. '!';
+
---
+++ demo_post2.php
@@ -0,0 +1,6 @@
+<?php
+echo "Hallo POST, ich bin " . $_POST["fname"] . " " . $_POST["lname"] . "!";
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+echo '<br>';
+echo 'Hallo, ' . $fname . ' ' .  $lname .'!';
